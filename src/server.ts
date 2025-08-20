50ceef3aae5cbef615a0815781b50026b8a6f24e
Lucas Jurgensen <114704161+lucas-jurgensen@users.noreply.github.com>
2025-08-20 00:39:54 -0300

    Mudando a roda do server.ts

--- src/server.ts
+++ src/server.ts
@@ -7,5 +7,5 @@ app.use(express.json());
 app.use("/", routes);
 
 app.listen(3000, () => {
-    console.log("Servidor rodando em https://localhost:3000/");
+    console.log("Servidor rodando em http://localhost:3000/");
 });
